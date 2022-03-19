cb5a686556a3b9a63ae3ef9ea6790636ede8a548
Bill Kayser <bill@kayser.org>
2022-03-18 20:06:25 -0700

    work on DnD

--- src/App.js
+++ src/App.js
@@ -5,28 +5,27 @@ import { data } from "./data"
 import Split from "react-split"
 import { nanoid } from "nanoid"
 
- /**
-     * Challenge: Try to figure out a way to display only the 
-     * first line of note.body as the note summary in the
-     * sidebar.
-     * 
-     * Hint 1: note.body has "invisible" newline characters
-     * in the text every time there's a new line shown. E.g.
-     * the text in Note 1 is:
-     * "# Note summary\n\nBeginning of the note"
-     * 
-     * Hint 2: See if you can split the string into an array
-     * using the "\n" newline character as the divider
-     */
-    
+/**
+    * Challenge: Try to figure out a way to display only the 
+    * first line of note.body as the note summary in the
+    * sidebar.
+    * 
+    * Hint 1: note.body has "invisible" newline characters
+    * in the text every time there's a new line shown. E.g.
+    * the text in Note 1 is:
+    * "# Note summary\n\nBeginning of the note"
+    * 
+    * Hint 2: See if you can split the string into an array
+    * using the "\n" newline character as the divider
+    */
+
 export default function App() {
     const [notes, setNotes] = React.useState(() => {
-        return(JSON.parse(localStorage.getItem("notes") || '[]'))
+        return (JSON.parse(localStorage.getItem("notes") || '[]'))
     })
 
     useEffect(
         () => {
-            console.log("Update items")
             localStorage.setItem("notes", JSON.stringify(notes))
         },
         [notes]
@@ -42,28 +41,70 @@ export default function App() {
             body: "# Type your markdown note's title here"
         }
         setNotes(prevNotes => [newNote, ...prevNotes])
-        setCurrentNoteId(newNote.id)
+//        setCurrentNoteId(newNote.id)
     }
 
     function updateNote(text) {
         setNotes(oldNotes => {
-            const newNotes = [ ...oldNotes ]
-            const currentNoteIndex = newNotes.map((m)=>m.id).indexOf(currentNoteId)
+            const newNotes = [...oldNotes]
+            newNotes[findNoteIndex(currentNoteId)].body = text;
+            /*
+            const currentNoteIndex = findNoteIndex(currentNoteId);
             newNotes.splice(currentNoteIndex, 1)
             newNotes.unshift({
                 id: currentNoteId,
                 body: text
             })
+            */
             return newNotes;
         })
     }
-
+    function findNoteIndex(noteId) {
+        return (notes.map((n) => n.id).indexOf(noteId))
+    }
     function findCurrentNote() {
         return notes.find(note => {
             return note.id === currentNoteId
         }) || notes[0]
     }
+    /**
+      * Challenge: complete and implement the deleteNote function
+      * 
+      * Hints: 
+      * 1. What array method can be used to return a new
+      *    array that has filtered out an item based 
+      *    on a condition?
+      * 2. Notice the parameters being based to the function
+      *    and think about how both of those parameters
+      *    can be passed in during the onClick event handler
+      */
 
+    function deleteNote(event, noteId) {
+        event.stopPropagation();
+        const i = findNoteIndex(noteId);
+        const newNoteList = [...notes]
+        newNoteList.splice(i, 1)
+        setNotes(newNoteList)
+        /**
+         * Change selected note if necessary
+         */
+        if (noteId == currentNoteId && newNoteList.length > 0) {
+            setCurrentNoteId(newNoteList[Math.min(newNoteList.length - 1, i)].id)
+        }
+    }
+
+    function swapNotes(fromId, toId) {
+        if (fromId == toId) return;
+        const fromIndex = findNoteIndex(fromId);
+        setNotes((oldNotes) => {
+            debugger
+            const newNotes = [ ...oldNotes ]
+            const target = newNotes[fromIndex]
+            newNotes.splice(fromIndex, 1)
+            newNotes.splice(findNoteIndex(toId), 0, target)
+            return(newNotes)
+        })
+    }
     return (
         <main>
             {
@@ -79,6 +120,8 @@ export default function App() {
                             currentNote={findCurrentNote()}
                             setCurrentNoteId={setCurrentNoteId}
                             newNote={createNewNote}
+                            deleteNote={deleteNote}
+                            swapAction={swapNotes}
                         />
                         {
                             currentNoteId &&
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,22 +1,50 @@
 import React from "react"
 
 export default function Sidebar(props) {
+
+    function dragestartHandler(event, noteId) {
+        event.dataTransfer.setData("text/plain", event.target.id)
+        event.dataTransfer.effectAllowed = "move";
+        console.log(`Start drag of "${event.target.id}"`)
+    }
+    function dropHandler(event) {
+        event.preventDefault();
+
+        const sourceId = event.dataTransfer.getData("text/plain");
+        let dropTarget = event.target;
+        let dropId = dropTarget.id;
+
+        while (dropId == "") {
+            dropTarget = dropTarget.parentElement;
+            dropId = dropTarget.id;
+        }
+        console.log(`Dropped ${sourceId} onto: ${dropId}`);
+        props.swapAction(sourceId, dropId);
+    }
+    function dragoverHandler(event) {
+        event.preventDefault();
+        event.dataTransfer.dropEffect = "move";
+    }
+    function extractTitle(body) {
+        const m = body.match(/^#*\s*(\S[^\n]*)/)
+        return m ? m[1] : null;
+    }
     const noteElements = props.notes.map((note, index) => (
         <div key={note.id}>
-            <div
-                
-                className={`title ${
-                    note.id === props.currentNote.id ? "selected-note" : ""
-                }`}
-                onClick={() => props.setCurrentNoteId(note.id)}
-            >
-                <h4 className="text-snippet">{extractTitle(note.body) || "Note "+(index + 1)}</h4>
+            <div className={`drag-me title ${note.id === props.currentNote.id ? "selected-note" : ""}`}
+                id={note.id}
+                onClick={() => props.setCurrentNoteId(note.id)} draggable="true" 
+                onDragStart={(ev)=>dragestartHandler(ev, note.id)}>
+                <div className="text-snippet">{extractTitle(note.body) || "Note " + (index + 1)}</div>
+                <button className="delete-btn" onClick={(event) => props.deleteNote(event, note.id)}>
+                  <i className="gg-trash trash-icon"></i>
+                </button>
             </div>
         </div>
     ))
 
     return (
-        <section className="pane sidebar">
+        <section className="pane sidebar" onDragOver={dragoverHandler} onDrop={dropHandler}>
             <div className="sidebar--header">
                 <h3>Notes</h3>
                 <button className="new-note" onClick={props.newNote}>+</button>
@@ -24,9 +52,4 @@ export default function Sidebar(props) {
             {noteElements}
         </section>
     )
-}
-
-function extractTitle(body) {
-    const m = body.match(/^#*\s*(\S[^\n]*)/)
-    return m ? m[1] : null;
 }
